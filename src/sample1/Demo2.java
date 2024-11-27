4266a4966f8cc81c50d44f25fd98f905fe999a5d
sushildahifale <sushildahifale100@gmail.com>
2024-11-27 17:53:26 +0530

    demo2 file created

--- src/sample1/Demo2.java
+++ src/sample1/Demo2.java
@@ -1,11 +1,12 @@
 package sample1;
 
-public class Demo1 {
+public class Demo2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hiiiiiiiiiiii");
 		
+		System.out.println("Byyyyyyyyyy");
+
 	}
 
 }
