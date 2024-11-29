2a66e006448ccf343488eb42d343d6c461d61a2b
sushildahifale <sushildahifale100@gmail.com>
2024-11-29 19:21:52 +0530

    dem5 created

---
+++ src/sample1/dem5.java
@@ -0,0 +1,15 @@
+package sample1;
+
+import org.testng.annotations.Test;
+
+public class dem5 {
+	
+
+    @Test
+    public void abc ()
+      {
+	      System.out.println("dem5 reunning"); 
+      }
+
+
+}
